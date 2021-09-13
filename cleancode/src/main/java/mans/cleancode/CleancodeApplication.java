6d775077fc796979502fb2edd4cf46cbaad79a85
Mans <hmw9309@gmail.com>
2021-09-13 11:26:15 +0900

    Init Spring

---
+++ cleancode/src/main/java/mans/cleancode/CleancodeApplication.java
@@ -0,0 +1,13 @@
+package mans.cleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleancodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleancodeApplication.class, args);
+	}
+
+}
